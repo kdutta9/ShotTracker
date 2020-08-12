c733cd073a6b830e92e326728c1064669e335831
Kunal Dutta <kunaldutta1749@gmail.com>
2020-08-12 11:47:08 -0700

    changed quantized field to false

--- app/src/main/java/org/tensorflow/lite/examples/detection/DetectorActivity.java
+++ app/src/main/java/org/tensorflow/lite/examples/detection/DetectorActivity.java
@@ -50,8 +50,8 @@ public class DetectorActivity extends CameraActivity implements OnImageAvailable
   private static final Logger LOGGER = new Logger();
 
   // Configuration values for the prepackaged SSD model.
-  private static final int TF_OD_API_INPUT_SIZE = 300;
-  private static final boolean TF_OD_API_IS_QUANTIZED = true;
+  private static final int TF_OD_API_INPUT_SIZE = 416;
+  private static final boolean TF_OD_API_IS_QUANTIZED = false;
   private static final String TF_OD_API_MODEL_FILE = "custom-416.tflite";
   private static final String TF_OD_API_LABELS_FILE = "file:///android_asset/labels.txt";
   private static final DetectorMode MODE = DetectorMode.TF_OD_API;
